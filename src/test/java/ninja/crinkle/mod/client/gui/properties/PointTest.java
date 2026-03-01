4dd59ce440c1a4668a3b5a279d689b0b407aac1e
Galen <fuzzy.little.devil@gmail.com>
2026-03-01 18:11:39 -0500

    Clean up a refactored file

--- src/test/java/ninja/crinkle/mod/client/gui/properties/PointTest.java
+++ src/test/java/ninja/crinkle/mod/client/gui/properties/PointTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Point Properties")
-class ImmutablePointTest {
+class PointTest {
 
     @Nested
     @DisplayName("Accessors")
